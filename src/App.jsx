e6ca7f4c15b141929b5f69c40742efd561ffd29e
ANIK365PRO <mohonto05@gmail.com>
2025-05-12 15:36:13 +0600

    add first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 class="text-3xl font-bold underline max-w-sm mx-auto">
+          Lets build dragon news!
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
