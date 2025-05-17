a54c01afa489dff447c563030756db863c1fdced
ANIK365PRO <mohonto05@gmail.com>
2025-05-17 18:55:35 +0600

    added CategoryNews and use useEffect hokks to get filteredNews

--- src/Components/Pages/CategoryNews.jsx
+++ src/Components/Pages/CategoryNews.jsx
@@ -3,16 +3,27 @@ import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
   const { id } = useParams();
+
   const data = useLoaderData();
   // console.log(id, data);
 
   const [categoryNews, setCategoryNews] = useState([]);
 
   useEffect(() => {
-    const filterdNews = data.filter(news => news.category_id == id);
-    console.log(filterdNews);
-
-    setCategoryNews(filterdNews);
+    if (id == '0') {
+      setCategoryNews(data);
+      return;
+    } else if (id == '1') {
+      const filteredNews = data.filter(
+        news => news.others.is_today_pick == true
+      );
+      console.log(filteredNews);
+      setCategoryNews(filteredNews);
+    } else {
+      const filteredNews = data.filter(news => news.category_id == id);
+      console.log(filteredNews);
+      setCategoryNews(filteredNews);
+    }
   }, [data, id]);
   return (
     <div>
